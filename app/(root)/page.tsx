8b443baf49383fe37553048ff76c63fc8f2ba10a
Kandy-Hamisi <hkandy2000@gmail.com>
2025-01-12 09:27:34 +0300

    finished the designing of the homepage

---
+++ app/(root)/page.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import BookList from "@/components/BookList";
+import BookOverview from "@/components/BookOverview";
+import { sampleBooks } from "@/constants";
+
+export default function Home() {
+  return (
+    <>
+      <BookOverview {...sampleBooks[0]} />
+      <BookList
+        title="Latest Books"
+        books={sampleBooks}
+        containerClassName="mt-28"
+      />
+    </>
+  );
+}
